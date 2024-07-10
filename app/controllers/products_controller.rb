eb937d29bb003d4ffd1608a12150a2cc1bd505e2
GabzbyDiaz <gabzky.diaz@gmail.com>
2024-07-10 16:14:19 -0500

    Setup product index and show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,18 @@
 class ProductsController < ApplicationController
   def index
+    @categories = Category.all
     @products = Product.all
 
     if params[:filter] == 'on_sale'
       @products = @products.on_sale
-    elsif params[:filter] == 'recently_updated'
-      @products = @products.recently_updated
+    elsif params[:category]
+      @products = @products.where(category_id: params[:category])
     end
+
+    @products = @products.order(created_at: :desc) if params[:sort] == 'latest'
+    @products = @products.order(name: :asc) if params[:sort] == 'a-z'
+
+    @products = @products.page(params[:page]).per(12) # Pagination
   end
 
   def show
