d0f8026ca6ee3850b2050bc72eee6c02a49b0693
GabzbyDiaz <gabzky.diaz@gmail.com>
2024-07-11 23:55:34 -0500

    Add about and contact pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,18 @@
 class PagesController < ApplicationController
+  before_action :set_categories, only: [:home, :about, :contact]
+
   def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  private
+
+  def set_categories
     @categories = Category.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :products, only: [:index, :show]
   get 'search', to: 'products#search', as: :search_products
   devise_for :admin_users, ActiveAdmin::Devise.config
